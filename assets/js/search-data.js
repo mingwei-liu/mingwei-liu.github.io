591da0558dee095823af1315d244210831a3b35a
mingwei-liu <lmwtclmwtc@163.com>
2026-02-16 08:03:17 +0000

    Deploying to gh-pages from @ mingwei-liu/mingwei-liu.github.io@439314b385feb7b35b1b9cdbd8211cd0b81bfb82 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-services",
           title: "services",
           description: "",
